ddb4f04212c3b360fd38585bd6f5886d7f6be8c9
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-08-30 14:45:03 +0500

    In this project i made double landing page with the help of html,css and javascript.In css i used backdrop-filter brightness property,(white-space) and how to write an element two classes one time .split.left. In javascript i used mouseenter and mouseleave add event listener.

--- DigitalClock(9)/index.js
+++ DigitalClock(9)/index.js
@@ -9,14 +9,14 @@ function updateClock(){
     let s = new Date().getSeconds();
     let ampm = "AM"
 
-    h = h < 10 ? "0"+ h : h;
-    m = m < 10 ? "0" + m : m;
-    s =s < 10 ? "0" + s : s;
-
     if(h >= 12){
         h = h - 12;
         ampm = "PM"
     }
+    
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s =s < 10 ? "0" + s : s;
     
     hourEl.innerText = h;
     minutesEl.innerText = m;
